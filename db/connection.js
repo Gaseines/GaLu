49471c4dc48c0f5a19cfc6a56a4bb2756a04b312
Gaseines <gaseinesdzn@hotmail.com>
2024-06-19 00:53:49 -0300

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('listas_ga_lu', 'root', '', {
+const sequelize = new Sequelize('gasein11_listas_ga_lu', 'gasein11_gsn', 'gaseines2003', {
     dialect: 'mysql',
     host: 'localhost'
 })
