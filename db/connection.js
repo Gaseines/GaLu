a1a10a1adfa6f75d04e4e5c0332edcc2fe7bbd61
Gaseines <gaseinesdzn@hotmail.com>
2024-06-19 11:29:36 -0300

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,11 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('gasein11_listas_ga_lu', 'gasein11_gsn', 'gaseines2003', {
-    dialect: 'mysql',
-    host: 'localhost'
+const sequelize = new 
+Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false
+    
 })
 
 module.exports = sequelize
